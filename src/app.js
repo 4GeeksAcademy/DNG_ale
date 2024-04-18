361601485c52bfbee3cd064729ba467d150d5006
Alejandrabalv <48587816+Alejandrabalv@users.noreply.github.com>
2024-04-18 11:31:00 +0000

    DNG aleboigues

--- src/app.js
+++ src/app.js
@@ -9,3 +9,34 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronouns = ["la", "nuestra"];
+let adjectives = ["gran", "pequeña"];
+let nouns = ["fábrica", "empresa"];
+let extensions = [".us", ".es", ".com", ".de"];
+
+function generateDomainNames(pronouns, adjectives, nouns, extensions) {
+  let domainNames = [];
+  for (let pronoun of pronouns) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let extension of extensions) {
+          let domainName = pronoun + adjective + noun + extension;
+          domainNames.push(domainName);
+        }
+      }
+    }
+  }
+  return domainNames;
+}
+
+let generatedDomains = generateDomainNames(
+  pronouns,
+  adjectives,
+  nouns,
+  extensions
+);
+
+for (let domain of generatedDomains) {
+  console.log(domain);
+}
